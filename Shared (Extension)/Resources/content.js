c0af0d8f90716a6b3c404e53e889136dc3ac9a85
larryhou <larryhou@foxmail.com>
2022-05-12 22:49:38 +0800

    Remove all Ads in BloomBerg news

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,3 +5,34 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+
+function removeAds(node) {
+    switch(node.nodeType) {
+        case Node.ELEMENT_NODE: {
+            var placeholder = node.getAttribute("data-ad-placeholder");
+            var root = node.getAttribute("data-component-root");
+            var id = node.getAttribute("class");
+            if (placeholder || root == "LeaderboardAd" || (id&&id.includes("page-ad"))) {
+                node.parentNode.removeChild(node);
+            }
+            
+            var child = node.firstChild;
+            while(child) {
+                var next = child.nextSibling;
+                removeAds(child);
+                child = next;
+            }
+        } break;
+    }
+}
+
+(function(node) {
+    removeAds(node);
+    var obs = new window.WebKitMutationObserver(function(e){
+        var num = 0;
+        e.forEach(rec=>rec.addedNodes.length&&num++);
+        if (num) {removeAds(node);}
+    });
+    obs.observe(node,{childList:true, subtree:true });
+})(document.body);
